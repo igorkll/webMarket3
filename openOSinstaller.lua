4dd035c8d378e10c3a99e4e250ca2091788c0e3b
igorkll <69525332+igorkll@users.noreply.github.com>
2022-05-30 12:01:31 +0300

    Update openOSinstaller.lua

--- openOSinstaller.lua
+++ openOSinstaller.lua
@@ -55,39 +55,50 @@ local function install(address, url)
         local fileUrl = url .. filePath
         local file, err = getInternetFile(fileUrl)
         if file then
-            status("saving file: " .. filePath)
+            status("Saving File: " .. filePath, -1)
             local ok, err = saveFile(component.proxy(address), filePath, file)
             if ok then
-                status("saved file: " .. filePath)
+                status("Saved File: " .. filePath, -1)
             else
-                status("error to save file: " .. (err or "unkown"))
+                status("Error To Save File: " .. (err or "Unkown"), -1)
             end
         else
-            status("error to get file: " .. (err or "unkown"))
+            status("Error To Get File: " .. (err or "Unkown"), -1)
         end
     end
 end
 
+local function selectBranch()
+    local branchs = {"Main", "Dev", "Back"}
+    local num = menu("Select Branch", branchs)
+    if num == #branchs then return nil end
+    return branchs[num]:lower()
+end
+
 while true do
-    local selected = selectfs("select fs to install")
+    local selected = selectfs("Select Fs To Install")
     if not selected then return end
 
     local fs = component.proxy(selected)
     local label = fs.getLabel()
     if fs.isReadOnly() then
-        status("filesystem is readonly", true)
+        status("Filesystem Is Readonly", true)
     else
-        local modes = {"openOS classic", "openOS modified", "mod only(no os)", "back"}
-        local num = menu("select distribution", modes)
-        if num ~= 4 and yesno("install " .. modes[num] .. " to " .. ((label and (label .. "-") or "") .. selected:sub(1, 3)) .. "?") then
-            if yesno("format?") then fs.remove("/") end
+        local modes = {"OpenOS Classic", "OpenOS Modified", "Mod Only(No Os)", "Back"}
+        local num = menu("Select Distribution", modes)
+        if num ~= 4 and yesno("Install " .. modes[num] .. " To " .. ((label and (label .. "-") or "") .. selected:sub(1, 3)) .. "?") then
+            if yesno("Format?") then fs.remove("/") end
             if num == 1 then
                 install(selected, "https://raw.githubusercontent.com/igorkll/openOS/main")
             elseif num == 2 then
+                local branch = selectBranch()
+                if not branch then return end
                 install(selected, "https://raw.githubusercontent.com/igorkll/openOS/main")
-                install(selected, "https://raw.githubusercontent.com/igorkll/openOSpath/main")
+                install(selected, "https://raw.githubusercontent.com/igorkll/openOSpath/" .. branch)
             elseif num == 3 then
-                install(selected, "https://raw.githubusercontent.com/igorkll/openOSpath/main")
+                local branch = selectBranch()
+                if not branch then return end
+                install(selected, "https://raw.githubusercontent.com/igorkll/openOSpath/" .. branch)
             end
         end
     end
